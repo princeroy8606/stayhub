433d230b01fa20afa7096e345ff7ce9fc4cf0f32
PRINCE ROY <83387075+princeroy8606@users.noreply.github.com>
2024-07-02 09:57:51 +0530

    reshape

--- client/src/components/owner/bookings/dateCalculator.js
+++
@@ -1,9 +0,0 @@
-export const dateCalculator = (dateSting) => {
-  const inputDate = new Date(dateSting);
-  const day = inputDate.getUTCDate();
-  const month = inputDate.getUTCMonth() + 1;
-  const year = inputDate.getUTCFullYear();
-  const formattedDay = String(day).padStart(2, "0");
-  const formattedMonth = String(month).padStart(2, "0");
-  return `${formattedDay}-${formattedMonth}-${year}`;
-};
